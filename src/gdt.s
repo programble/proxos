2f577e85bea5bdccbf58a175f6287e271b97f34e
Curtis (Programble) <programble@gmail.com>
2010-07-18 23:20:04 -0400

    Split up loader.s into multiple ASM files

--- src/gdt.s
+++ src/gdt.s
@@ -15,39 +15,18 @@
 ; You should have received a copy of the GNU General Public License
 ; along with Proxos.  If not, see <http://www.gnu.org/licenses/>.
 
-global loader
-extern kmain ; Defined in main.c
+global gdt_flush
+extern gp ; Defined in gdt.c
 
-; Multiboot setup
-MODULEALIGN equ 1<<0
-MEMINFO equ 1<<1
-FLAGS equ MODULEALIGN | MEMINFO
-MAGIC equ 0x1BADB002
-CHECKSUM equ -(MAGIC + FLAGS)
+gdt_flush:
+    lgdt [gp] ; Load our own GDT
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    jmp 0x08:flush2
 
-section .text
-align 4
-MultiBootHeader:
-    dd MAGIC
-    dd FLAGS
-    dd CHECKSUM
-
-STACKSIZE equ 0x4000
-
-loader:
-    mov esp, stack+STACKSIZE ; Set up the stack
-    push eax ; Multiboot magic
-    push ebx ; Multiboot info
-
-    call kmain ; Call the C kernel
-
-    cli
-
-hang:
-    hlt
-    jmp hang
-
-section .bss
-align 4
-stack:
-    resb STACKSIZE
+flush2:
+    ret ; Return back to C
--- src/idt.s
+++ src/idt.s
@@ -15,39 +15,9 @@
 ; You should have received a copy of the GNU General Public License
 ; along with Proxos.  If not, see <http://www.gnu.org/licenses/>.
 
-global loader
-extern kmain ; Defined in main.c
+global idt_load
+extern idtp ; Defined in idt.c
 
-; Multiboot setup
-MODULEALIGN equ 1<<0
-MEMINFO equ 1<<1
-FLAGS equ MODULEALIGN | MEMINFO
-MAGIC equ 0x1BADB002
-CHECKSUM equ -(MAGIC + FLAGS)
-
-section .text
-align 4
-MultiBootHeader:
-    dd MAGIC
-    dd FLAGS
-    dd CHECKSUM
-
-STACKSIZE equ 0x4000
-
-loader:
-    mov esp, stack+STACKSIZE ; Set up the stack
-    push eax ; Multiboot magic
-    push ebx ; Multiboot info
-
-    call kmain ; Call the C kernel
-
-    cli
-
-hang:
-    hlt
-    jmp hang
-
-section .bss
-align 4
-stack:
-    resb STACKSIZE
+idt_load:
+    lidt [idtp]
+    ret
